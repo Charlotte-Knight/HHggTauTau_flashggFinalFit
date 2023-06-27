36c62ba93ddca54077410bc258767a541f83f08d
francesco setti <fsetti@ucsb.edu>
2023-06-27 10:30:11 -0700

    add HH plotting scripts

--- get_limit_systematics.sh
+++ get_limit_systematics.sh
@@ -5,7 +5,7 @@ set -x
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /vols/grid/cms/setup.sh
 
-tag=18Nov2022_fullRun2
+tag=09May2023_nonRes_SM_final
 trees=/home/users/fsetti/ic_flashgg/CMSSW_10_2_13/src/flashggFinalFit/files_systs/$tag/
 
 cmsenv
@@ -32,7 +32,7 @@ model_sig(){
 	#procs=("ttH")
 
 	for year in 2016 2017 2018
-	#for year in 2016
+	#for year in 2018
 	do
 		rm -rf $trees/ws_signal_$year
 		mkdir -p $trees/ws_signal_$year
@@ -90,31 +90,31 @@ make_datacard(){
 	 python RunYields.py --inputWSDirMap 2016=${trees}/ws_signal_2016,2017=${trees}/ws_signal_2017,2018=${trees}/ws_signal_2018 --cats auto --procs auto --batch local --mergeYears --ext $tag --doSystematics --skipZeroes 
 	 python makeDatacard.py --years 2016,2017,2018 --ext $tag --prune --pruneThreshold 0.000001 --doSystematics
 	 mv Datacard.txt Datacard_$tag.txt
-	 #python RunYields.py --inputWSDirMap 2018=${trees}/ws_signal_2018 --cats "SR1" --procs "HHggTauTau" --batch local --ext $tag --doSystematics --skipZeroes 
-	 #python makeDatacard.py --years 2018 --ext $tag --prune --pruneThreshold 0.000001 --doSystematics
 
 	popd
 }
 
 run_combine(){
 	pushd Combine
-		rm -rf Models
-		mkdir -p Models
-		mkdir -p Models/signal
-		mkdir -p Models/background
-		cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
-		cp ../Background/outdir_$tag/CMS-HGG*.root ./Models/background/
+		#rm -rf Models
+		#mkdir -p Models
+		#mkdir -p Models/signal
+		#mkdir -p Models/background
+		#cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
+		#cp ../Background/outdir_$tag/CMS-HGG*.root ./Models/background/
 		cp ../Datacard/Datacard_$tag.txt Datacard.txt
 	
 		python RunText2Workspace.py --mode  ggtt_resBkg_syst --dryRun
 		./t2w_jobs/t2w_ggtt_resBkg_syst.sh
 
-		combine --expectSignal 1 -t -1 --redefineSignalPOI r --cminDefaultMinimizerStrategy 0 -M AsymptoticLimits -m 125 -d Datacard_ggtt_resBkg_syst.root -n _AsymptoticLimit_r --freezeParameters MH --run=blind > combine_results_${tag}.txt
-		#combine --expectSignal 1 -t -1 --redefineSignalPOI r --cminDefaultMinimizerStrategy 0 -M MultiDimFit --algo grid --points 100 -m 125 -d Datacard_ggtt_resBkg_syst.root -n _Scan_r --freezeParameters MH --rMin 0 --rMax 5
+		combine --redefineSignalPOI r --cminDefaultMinimizerStrategy 0 -M AsymptoticLimits -m 125 -d Datacard_ggtt_resBkg_syst.root -n _AsymptoticLimit_r --freezeParameters MH --saveWorkspace  > combine_results_${tag}_unblind.txt
+		#combine --expectSignal 1 -t -1 --redefineSignalPOI r --cminDefaultMinimizerStrategy 0 -M AsymptoticLimits -m 125 -d Datacard_ggtt_resBkg_syst.root -n _AsymptoticLimit_r --freezeParameters MH --run=blind > combine_results_${tag}.txt
+		#combine --expectSignal 1 -t -1 --redefineSignalPOI r --cminDefaultMinimizerStrategy 0 -M MultiDimFit --algo grid --points 100 -m 125 -d Datacard_ggtt_resBkg_syst.root -n _Scan_r --freezeParameters MH --rMin 15 --rMax 27 > combine_results_${tag}_scan.txt
 		#python plotLScan.py higgsCombine_Scan_r.MultiDimFit.mH125.root
 		#cp NLL_scan* /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/$tag/
 
 		tail combine_results_${tag}.txt
+		#tail combine_results_${tag}_scan.txt
 	popd	
 }
 
@@ -122,15 +122,16 @@ syst_plots(){
 	pushd Combine
 	
 		#text2workspace.py Datacard.txt -m 125
-		#combineTool.py  --expectSignal 1 -t -1 -M Impacts -d Datacard.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -5 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --doInitialFit --robustFit 1 --robustHesse 1
-		#combineTool.py  --expectSignal 1 -t -1 -M Impacts -d Datacard.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -5 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --robustFit 1 --robustHesse 1 --doFits --parallel 10
 
-		#combineTool.py  -t -1 --setParameters r=23.0 -M Impacts -d Datacard.root --redefineSignalPOI r --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --doInitialFit --robustFit 1 --robustHesse 1
-		#combineTool.py  -t -1 --setParameters r=23.0 -M Impacts -d Datacard.root --redefineSignalPOI r --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --robustFit 1   --robustHesse 1 --doFits --parallel 10
+		#combineTool.py  -t -1 --setParameters r=23 -M Impacts -d Datacard.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -40 --rMax 100 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --doInitialFit --robustFit 1 --robustHesse 1
+		#combineTool.py  -t -1 --setParameters r=23 -M Impacts -d Datacard.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -40 --rMax 100 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --robustFit 1   --robustHesse 1 --doFits --parallel 10
 
-		#combineTool.py -M Impacts -d Datacard.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -5 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH -o impacts.json 
+		combineTool.py  --setParameters r=23 -M Impacts -d Datacard_ggtt_resBkg_syst.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -40 --rMax 100 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --doInitialFit --robustFit 1 --robustHesse 1
+		combineTool.py  --setParameters r=23 -M Impacts -d Datacard_ggtt_resBkg_syst.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -40 --rMax 100 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH --robustFit 1   --robustHesse 1 --doFits --parallel 10
 
-		plotImpacts.py -i impacts.json -o impacts
+		combineTool.py -M Impacts -d Datacard_ggtt_resBkg_syst.root --redefineSignalPOI r --autoMaxPOIs "r" --rMin -40 --rMax 100 --squareDistPoiStep --cminDefaultMinimizerStrategy 0 -m 125 --freezeParameters MH -o impacts.json 
+
+		plotImpacts.py -i impacts.json -o impacts --blind
 		mkdir -p /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/$tag/
 		cp impacts.pdf /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/$tag/impacts.pdf
 	popd	
@@ -152,23 +153,22 @@ bias_study(){
 	mkdir -p /home/users/fsetti/public_html/HH2ggtautau/bias_study/${tag}
 	mkdir -p /home/users/fsetti/public_html/HH2ggtautau/bias_study/${tag}/${sr}
 
-	#pushd Combine
-	#	rm -rf Models
-	#	mkdir -p Models
-	#	mkdir -p Models/signal
-	#	mkdir -p Models/background
-	#	cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
-	#	cp ../Background/outdir_$tag/CMS-HGG*.root ./Models/background/
-	#popd
+	pushd Combine
+		rm -rf Models
+		mkdir -p Models
+		mkdir -p Models/signal
+		mkdir -p Models/background
+		cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
+		cp ../Background/outdir_$tag/CMS-HGG*.root ./Models/background/
+	popd
 
 	srs=("SR1" "SR2")
-	#srs=("SR2")
+	#srs=("SR1")
 	for sr in "${srs[@]}"
 	do
 		pushd Datacard	
 			rm -rf yields_${tag}_${sr}
 			python RunYields.py --inputWSDirMap 2016=${trees}/ws_signal_2016,2017=${trees}/ws_signal_2017,2018=${trees}/ws_signal_2018 --cats ${sr} --procs auto --batch local --mergeYears --skipZeroes --ext ${tag}_${sr} --doSystematics 
-			#python RunYields.py --inputWSDirMap 2016=${trees}/ws_signal_2016,2017=${trees}/ws_signal_2017,2018=${trees}/ws_signal_2018 --cats ${sr} --procs "HHggTauTau,HHggWWdileptonic,HHggWWsemileptonic"  --batch local --mergeYears --skipZeroes --ext ${tag}_${sr} --doSystematics 
 			python makeDatacard.py --years 2016,2017,2018 --ext ${tag}_${sr} --prune --pruneThreshold 0.00001 --doSystematics
 			#python prepareDatacard.py
 			#mv Datacard_tmp.txt ../Combine/Datacard_${sr}.txt
@@ -187,13 +187,13 @@ bias_study(){
 
 			rm -rf Bias*
 
-			./RunBiasStudy.py -d ../Datacard_${sr}.root -t -n 10000
-			#./RunBiasStudy.py -d ../Datacard_${sr}.root -f -c "--cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --freezeParameters MH --rMin -100 --rMax 100" 
-			./RunBiasStudy.py -d ../Datacard_${sr}.root -f -c "--cminDefaultMinimizerStrategy 0 --freezeParameters MH --rMin -70 --rMax 100" -n 10000
-			./RunBiasStudy.py -d ../Datacard_${sr}.root -p --gaussianFit -n 10000
+			./RunBiasStudy.py -d ../Datacard_${sr}.root -t -n 10000 -e 23
+			#./RunBiasStudy.py -d ../Datacard_${sr}.root -f -c "--cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --freezeParameters MH --rMin -100 --rMax 100" -n 5000 -e 23
+			./RunBiasStudy.py -d ../Datacard_${sr}.root -f -c "--cminDefaultMinimizerStrategy 0 --freezeParameters MH --rMin -40 --rMax 100" -n 10000 -e 23
+			./RunBiasStudy.py -d ../Datacard_${sr}.root -p --gaussianFit -n 10000 -e 23
 
 			cp -r BiasPlots /home/users/fsetti/public_html/HH2ggtautau/bias_study/${tag}/${sr}/
-			cp /home/users/fsetti/public_html/HH2ggtautau/niceplots/index.php /home/users/fsetti/public_html/HH2ggtautau/bias_study/${tag}/${sr}/BiasPlots/
+			cp /home/users/fsetti/public_html/HH2ggtautau/niceplots/index.php /home/users/fsetti/public_html/HH2ggtautau/bias_study/${tag}/${sr}/
 
 			cd ..
 		popd
@@ -204,7 +204,7 @@ bias_study(){
 #model_bkg
 #model_sig
 #make_datacard
-run_combine
+#run_combine
 #syst_plots
-#copy_plot
+copy_plot
 #bias_study
