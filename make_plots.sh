bf8204a8f31499fb62897e4a5498b0e2a4348b0b
francesco setti <fsetti@ucsb.edu>
2023-07-11 17:25:11 -0700

    add plot script

--- make_plots.sh
+++ make_plots.sh
@@ -5,18 +5,18 @@ set -x
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /vols/grid/cms/setup.sh
 
-tag=18Nov2022_fullRun2
-trees=/home/users/fsetti/ic_flashgg/CMSSW_10_2_13/src/flashggFinalFit/files_systs/$tag/
+tag=09May2023_nonRes_SM_final
 
 cmsenv
 source setup.sh
 
-nToys=500
+nToys=1000
 
 make_toys(){
 	pushd Plots 
 		rm -rf SplusBModels$tag
-		python makeToys.py --inputWSFile ../Combine/Datacard.root --ext $tag --dryRun --nToys $nToys
+		#python makeToys.py --inputWSFile ../Combine/Datacard_bkgOnly.root --ext $tag --dryRun --nToys $nToys
+		python makeToys.py --inputWSFile ../Combine/Datacard_ggtt_resBkg_syst.root --ext $tag --dryRun --nToys $nToys
 		iter=0
 		while [ $iter -lt $nToys ]
 		do			
@@ -37,11 +37,41 @@ make_toys(){
 
 make_SpB(){
 	pushd Plots 
-		python makeSplusBModelPlot.py --inputWSFile ../Combine/Datacard_ggtt_resBkg_syst.root --cat "SR1" --doBands --ext $tag --parameterMap ".*/HH2ggtautau.*:r[1,0,2]"
+		#python makeSplusBModelPlot.py --inputWSFile ../Combine/Datacard_bkgOnly.root --cat "all" --doBands --ext $tag --doResonantBackground
+		python makeSplusBModelPlot.py --inputWSFile ../Combine/Datacard_ggtt_resBkg_syst.root --cat "all" --doBands --ext $tag --doResonantBackground --unblind --parameterMap r:10
 
-		cp SplusBModels$tag/SR1_CMS_hgg_mass.png /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels/SR1_CMS_hgg_mass.png
+		mkdir -p /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag
+		mkdir -p /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels
+
+		cp SplusBModels$tag/SR1_CMS_hgg_mass.png /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels/SR1_CMS_hgg_mass_r10.png
+		cp SplusBModels$tag/SR2_CMS_hgg_mass.png /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels/SR2_CMS_hgg_mass_r10.png
+		cp SplusBModels$tag/SR1_CMS_hgg_mass.pdf /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels/SR1_CMS_hgg_mass_r10.pdf
+		cp SplusBModels$tag/SR2_CMS_hgg_mass.pdf /home/users/fsetti/public_html/HH2ggtautau/flashggFinalFit/Plots/$tag/SplusBModels/SR2_CMS_hgg_mass_r10.pdf
 	popd	
 
 }
 
-make_SpB
+make_plot_ws(){
+	pushd Combine
+		#rm -rf Models
+		#mkdir -p Models
+		#mkdir -p Models/signal
+		#mkdir -p Models/background
+		#cp ../Signal/outdir_packaged/CMS-HGG*.root ./Models/signal/
+		#cp ../Background/outdir_$tag/CMS-HGG*.root ./Models/background/
+		cp ../Datacard/Datacard_$tag.txt Datacard.txt
+		#echo "r_singleHiggs rateParam * VH_*_hgg 1" >> Datacard.txt
+		#echo "r_singleHiggs rateParam * ttH_*_hgg 1" >> Datacard.txt
+		#echo "r_singleHiggs rateParam * ggH_*_hgg 1" >> Datacard.txt
+		#echo "r_singleHiggs rateParam * VBFH_*_hgg 1" >> Datacard.txt
+		#echo "nuisance edit freeze r_singleHiggs" >> Datacard.txt
+	
+		python RunText2Workspace.py --mode  plots --dryRun
+		./t2w_jobs/t2w_plots.sh
+	popd
+}
+
+
+#make_toys
+#make_SpB
+make_plot_ws
